77d5397017f402527e1aafc7a0bc5e817e93b8e4
richelbilderbeek <richel@richelbilderbeek.nl>
2016-09-09 13:22:41 +0200

    Added clean script, build fparser

--- build_other_githubs.sh
+++ build_other_githubs.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd ../RibiLibraries
 ./Apfloat.sh
+./fparser.sh
---
+++ clean.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+rm *.o
+rm *.gcda
+rm *.gcno
+rm *.gcna
+rm *.gcov
+rm Makefile
+rm *.pro.user
+rm *.gcda
+rm ui_*
+rm moc_*
+rm qrc_*
+rm AsciiArterConsole
+rm AsciiArterConsoleTest
+rm AsciiArterDesktop
